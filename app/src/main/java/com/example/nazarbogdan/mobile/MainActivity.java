d8103ac44f353a33b250e0b2bbce8846352d4673
Bogdan <bohdannazar2499@gmail.com>
2018-10-03 14:30:49 +0300

    Add lab 4

--- app/src/main/java/com/example/nazarbogdan/mobile/MainActivity.java
+++ app/src/main/java/com/example/nazarbogdan/mobile/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.nazarbogdan.mobile;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText firstNameEditText, lastNameEditText, emailEditText;
     EditText phoneEditText, passwordEditText, passwordConfirmEditText;
-    Button submitButton;
-
+    Button submitButton, newAct;
+    SharedPreferences mPrefs;
     String firstName, lastName, email;
     String phone, password, confirmPassword;
 
@@ -27,6 +33,23 @@ public class MainActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         passwordConfirmEditText = findViewById(R.id.passwordConfirmEditText);
         submitButton = findViewById(R.id.submitButton);
+        newAct = findViewById(R.id.new_act);
+        newAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    private void saveData(User user){
+        SharedPreferences mPrefs = getSharedPreferences("user", Context.MODE_PRIVATE);
+        SharedPreferences.Editor prefsEditor = mPrefs.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(user);
+        prefsEditor.putString("user", json);
+        prefsEditor.commit();
     }
 
     public void submitData(View view) {
@@ -63,7 +86,20 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+        else {
+
+            User user = new User(firstName, lastName, phone, email);
+            saveData(user);
+            firstNameEditText.setText("");
+            lastNameEditText.setText("");
+            phoneEditText.setText("");
+            emailEditText.setText("");
+            passwordEditText.setText("");
+            passwordConfirmEditText.setText("");
+            Toast.makeText(getApplicationContext(), "Дані збережено", Toast.LENGTH_LONG).show();
+        }
 
     }
+
 
 }
